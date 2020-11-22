35d8f5d3ccaf9c2fe0f43a2d563065bd63428bf3
nikmk <41271779+nikmk@users.noreply.github.com>
2020-11-23 05:27:47 +0530

    make it better 2b

--- server.js
+++ server.js
@@ -45,7 +45,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser())
 
-
+app.use(csrfProtection)
+app.get('/api/csrf-token',(req,res)=>{
+  res.json({csrfToken : req.csrfToken()})
+})
 
 app.post('/api/authenticate', async (req, res) => {
   try {
@@ -194,10 +197,7 @@ const checkJwt = jwt({
   getToken: req => req.cookies.token
 })
 
-app.use(csrfProtection)
-app.get('/api/csrf-token',(req,res)=>{
-  res.json({csrfToken : req.csrfToken()})
-})
+
 
 // app.use('/api/payment', checkJwt,paymentRoutes)
 
