378a6b5c10b363c1bf5be2c22073a375b56b2c27
nikmk <41271779+nikmk@users.noreply.github.com>
2020-11-23 13:25:35 +0530

    csrf location changed

--- server.js
+++ server.js
@@ -45,10 +45,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser())
 
-app.use(csrfProtection)
-app.get('/api/csrf-token',(req,res)=>{
-  res.json({csrfToken : req.csrfToken()})
-})
 
 app.post('/api/authenticate', async (req, res) => {
   try {
@@ -197,6 +193,10 @@ const checkJwt = jwt({
   getToken: req => req.cookies.token
 })
 
+app.use(csrfProtection)
+app.get('/api/csrf-token',(req,res)=>{
+  res.json({csrfToken : req.csrfToken()})
+})
 
 
 // app.use('/api/payment', checkJwt,paymentRoutes)
